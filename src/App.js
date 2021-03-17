0bc348953ab7f2db4ea4c1bdbcb94a5d46cd4d42
Andrew Davis <andrewtdavis@hotmail.co.uk>
2021-03-17 12:23:57 +0000

    before axios api

--- src/App.js
+++ src/App.js
@@ -1,25 +1,107 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState, useEffect} from "react";
+import JobBoardComponent from './compnents/JobBoardComponent';
+import data from './assets/data.json';
+
+
 
 function App() {
+   const [jobs, setJobs] = useState([]);
+   const [filters, setFilters] = useState([]);
+
+   useEffect(() => { 
+      setJobs(data);  
+   }, [])
+
+   
+
+   
+
+   const filterFunction = ({role, level, tools, languages}) => {
+    const tags = [role, level];
+
+
+    if (filters.length === 0) {
+      return true;
+    }
+
+    if (tools) {
+        tags.push(...tools);
+    }
+
+    if (languages) {
+        tags.push(...languages)
+    }
+    
+    return tags.some(tag => filters.includes(tag));
+   };
+
+   const handleTagClick = (tag) => {
+     // Avoid re adding the tag
+     if (filters.includes(tag)) return;
+
+      setFilters([...filters, tag]);
+   }
+
+   const handleFilterClick = (passedFilter) => {
+      setFilters(filters.filter((f) => f !== passedFilter))
+   }
+
+   const clearFilters = () => {
+     setFilters([]);
+   }
+
+   const filteredJobs = jobs.filter(filterFunction);
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header className='bg-blue-200 mb-12'>
+        <img src='/images/bg-header-desktop.svg' alt='bg-image' />
       </header>
+      <div className='container m-auto'>
+        {
+          filters.length > 0 && (
+          <div className='flex flex-wrap bg-white shadow-md my-16 mx-10 p-4 rounded
+         sm:flex-row'>
+          {filters.map((filter) => (
+            <div className='m-2 my-4' >
+              <span 
+              className='text-blue-400 bg-blue-200 font-bold pl-2 py-2 rounded'>
+              {filter}
+              <span className=' p-2 text-white  bg-blue-400 cursor-pointer ml-2 mr-0 rounded-r' onClick={() => 
+              handleFilterClick(filter)}>×</span>
+              </span>  
+            </div> 
+          ))}
+          <button className='font-bold text-gray-300 m-2' onClick={clearFilters}>Clear</button>
+          </div>
+          )}
+      {
+        jobs.length === 0 ? (
+          <p>Jobs are fetching...</p>
+        ) : (
+          filteredJobs.map(job => (
+            <JobBoardComponent 
+              job={job}  
+              key={job.id} 
+              handleTagClick={handleTagClick}
+            />
+          ))
+        )
+      }
+      </div>
     </div>
   );
 }
 
 export default App;
+
+// TODOs
+// 1. Study the design & json *
+// 2. Create the Job board component *
+// 3. Get the data from json *
+// 4. Pass down data to JBC *
+// 5. Style it *
+// 5.b. Style mobile 
+// 6. Filter component
+// 7. Filter data
---
+++ src/compnents/JobBoardComponent.js
@@ -0,0 +1,44 @@
+import React from 'react';
+
+const JobBoardComponent = ({job, handleTagClick}) => {
+    const tags = [job.role, job.level];
+
+    if (job.tools) {
+        tags.push(...job.tools);
+    }
+
+    if (job.languages) {
+        tags.push(...job.languages)
+    }
+
+    return (
+    <div className={`flex flex-col bg-white shadow-md my-16 mx-10 p-4 rounded
+        ${ job.featured && 'border-l-4 border-black border-blue-400 border-solid'} sm:flex-row sm:my-4`}>
+        <div className='sm:my-auto'>
+            <img className='-mt-14 mb-4  w-20  sm:mt-0 sm:mr-4' src={job.logo} alt={job.company}></img>
+        </div>
+        <div className='flex flex-col justify-between  p-2 pb-6'>
+            <h3 className='font-bold mb-2 text-blue-400'>
+                {job.company}
+                {job.new && 
+                (<span className='text-white text-sm bg-blue-400 font-bold m-2 ml-4 py-1 px-2 rounded-full'>NEW</span>)
+                }
+                {job.featured && 
+                (<span className='text-white text-sm bg-gray-800 font-bold m-2 py-1 px-2 rounded-full'>FEATURED</span>)
+                }
+            </h3>
+            <h2 className='font-bold mb-2 text-xl'>{job.position}</h2>
+            <p className='text-gray-500'>{job.postedAt} · {job.contract} · {job.location}</p>
+        </div>
+        <div className='flex flex-wrap items-center  pt-4 border-t-2 border-gray-200 border-solid 
+                        sm:ml-auto sm:border-0 sm:pt-0 sm:mt-0'>
+            {tags.map((tag) => 
+            <span onClick={() => handleTagClick(tag)} className='text-blue-400 bg-blue-200 font-bold cursor-pointer m-2 p-2 rounded'>{tag}</span>)} 
+            
+        </div>
+    </div>
+    
+    )}
+
+
+export default JobBoardComponent;
